560ba6ab1aae7a7cbc7f44c40056541468ad2211
LEEJAEBIN1 <41048775+LEEJAEBIN1@users.noreply.github.com>
2020-10-06 16:16:42 +0900

    FineWorker

---
+++ FineWorker/FineWorker/FineWorker/compass/compass.ino
@@ -0,0 +1,145 @@
+#include "Wire.h"
+#include "I2Cdev.h"
+#include "MPU9250.h"
+MPU9250 accelgyro;
+I2Cdev   I2C_M;
+
+uint8_t buffer_m[6];
+
+int16_t ax, ay, az;
+int16_t gx, gy, gz;
+int16_t   mx, my, mz;
+int finalz = 0;
+float heading;
+float tiltheading;
+float Axyz[3];
+float Gxyz[3];
+float Mxyz[3];
+#define sample_num_mdate  2500
+volatile float mx_sample[3];
+volatile float my_sample[3];
+volatile float mz_sample[3];
+
+static float mx_centre = 0;
+static float my_centre = 0;
+static float mz_centre = 0;
+
+volatile int mx_max = 0;
+volatile int my_max = 0;
+volatile int mz_max = 0;
+
+volatile int mx_min = 0;
+volatile int my_min = 0;
+volatile int mz_min = 0;
+
+float temperature;
+float pressure;
+float atm;
+float altitude;
+
+void setup()
+{
+    Wire.begin();
+    Serial.begin(38400);
+    accelgyro.initialize();
+    get_calibration_Data();
+    delay(1000);
+}
+
+
+
+void loop()
+{
+    getCompassDate_calibrated(); 
+    getHeading();           
+    Serial.println(finalz);
+    delay(100);
+
+}
+
+void getHeading(void)
+{
+    heading = 180 * atan(Mxyz[1]/Mxyz[0]) / PI;
+    if(Mxyz[0] >= 0 && Mxyz[1] >0)
+    {
+      finalz = heading;
+    }
+    else if(Mxyz[0] < 0 && Mxyz >= 0)
+    {
+      finalz = 180+heading;
+    }
+    else if(Mxyz[0] <= 0 && Mxyz[1] < 0)
+    {
+      finalz = 180+heading;
+    }
+    else if(Mxyz[0] >= 0 && Mxyz[1] <= 0)
+    {
+      finalz = 360+heading;
+    }
+}
+void Mxyz_init_calibrated ()
+{
+    get_calibration_Data ();
+}
+
+
+void get_calibration_Data ()
+{
+    for (int i = 0; i < sample_num_mdate; i++)
+    {
+        get_one_sample_date_mxyz();
+
+        if (mx_sample[2] >= mx_sample[1])mx_sample[1] = mx_sample[2];
+        if (my_sample[2] >= my_sample[1])my_sample[1] = my_sample[2]; //find max value
+        if (mz_sample[2] >= mz_sample[1])mz_sample[1] = mz_sample[2];
+
+        if (mx_sample[2] <= mx_sample[0])mx_sample[0] = mx_sample[2];
+        if (my_sample[2] <= my_sample[0])my_sample[0] = my_sample[2]; //find min value
+        if (mz_sample[2] <= mz_sample[0])mz_sample[0] = mz_sample[2];
+
+    }
+
+    mx_max = mx_sample[1];
+    my_max = my_sample[1];
+    mz_max = mz_sample[1];
+
+    mx_min = mx_sample[0];
+    my_min = my_sample[0];
+    mz_min = mz_sample[0];
+
+    mx_centre = (mx_max + mx_min) / 2;
+    my_centre = (my_max + my_min) / 2;
+    mz_centre = (mz_max + mz_min) / 2;
+}
+
+void get_one_sample_date_mxyz()
+{
+    getCompass_Data();
+    mx_sample[2] = Mxyz[0];
+    my_sample[2] = Mxyz[1];
+    mz_sample[2] = Mxyz[2];
+}
+
+
+void getCompass_Data(void)
+{
+    I2C_M.writeByte(MPU9150_RA_MAG_ADDRESS, 0x0A, 0x01); //enable the magnetometer
+    delay(10);
+    I2C_M.readBytes(MPU9150_RA_MAG_ADDRESS, MPU9150_RA_MAG_XOUT_L, 6, buffer_m);
+
+    mx = ((int16_t)(buffer_m[1]) << 8) | buffer_m[0] ;
+    my = ((int16_t)(buffer_m[3]) << 8) | buffer_m[2] ;
+    mz = ((int16_t)(buffer_m[5]) << 8) | buffer_m[4] ;
+
+    Mxyz[0] = (double) mx * 1200 / 4096;
+    Mxyz[1] = (double) my * 1200 / 4096;
+    Mxyz[2] = (double) mz * 1200 / 4096;
+}
+
+void getCompassDate_calibrated ()
+{
+    getCompass_Data();
+    Mxyz[0] = Mxyz[0] - mx_centre;
+    Mxyz[1] = Mxyz[1] - my_centre;
+    Mxyz[2] = Mxyz[2] - mz_centre;
+}
---
+++ FineWorker/FineWorker/FineWorker/degree/degree.ino
@@ -0,0 +1,65 @@
+﻿#include <SoftwareSerial.h>
+#include "Wire.h"
+#include "MPU9250.h"
+#include "I2Cdev.h"
+
+SoftwareSerial BTSerial(3,2);//rx = 2번 tx = 3번
+
+#define pi 3.141592
+#define RADIANS_TO_DEGREES 180/3.14159
+#define fs 131.0;
+MPU9250 mpu;
+int a0 = 0;
+int Degree = 0;
+int State = 0;
+void Func_walking();
+void Check_degree();
+
+
+int16_t ax,ay,az;
+int16_t gx,gy,gz;
+int16_t mx,my,mz;
+float gyro_z;
+float accel_x;
+//자이로센서 바이어스값
+float base_gx=0, base_gy=0, base_gz=0;
+unsigned long pre_msec=0;
+void calibrate()
+{  
+  int loop =10;
+  for (int i=0;i<loop;i++)
+  {
+    mpu.getMotion9(&ax,&ay,&az,&gx,&gy,&gz,&mx,&my,&mz);
+    base_gx += gx;
+    base_gy += gy;
+    base_gz += gz;
+    delay(200);
+  }
+  base_gx /=loop;
+  base_gy /=loop;
+  base_gz /=loop;
+}
+ 
+ 
+void setup() {
+  Wire.begin();
+  BTSerial.begin(9600);
+  Serial.begin(9600);  
+  mpu.initialize();
+  calibrate(); 
+}
+void Check_degree()
+{
+    float dt = (millis()-pre_msec)/1000.0;
+    pre_msec = millis();
+    mpu.getMotion9(&ax,&ay,&az,&gx,&gy,&gz,&mx,&my,&mz);
+    accel_x = atan(ay/sqrt(pow(ax,2) + pow(az,2)))*RADIANS_TO_DEGREES;
+    gyro_z = (gz-base_gz)/fs;
+}
+void loop() {
+  Check_degree();
+  Serial.print((int)accel_x);
+  Serial.print("\t");
+  Serial.println((int)gyro_z);
+  BTSerial.write("12");
+}
